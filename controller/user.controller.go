12feef491b0aa1cb6091b2bfd214244f381fc9fb
Havock48 <adit.github@gmail.com>
2025-02-01 12:02:51 +0100

    update user data added

--- controller/user.controller.go
+++ controller/user.controller.go
@@ -3,7 +3,9 @@ package controller
 import (
 	"go-rest-api/config"
 	"go-rest-api/models"
+	"go-rest-api/utils"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -20,7 +22,63 @@ func GetUser(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"status": "success", "data": user})
 }
 
-func UpdateUser(c *gin.Context) {}
+func UpdateUser(c *gin.Context) {
+	userID := c.MustGet("userID").(uint)
+
+	var body struct {
+		Name *string `json:"name" bindng:"omitempty",min=2,max=100`
+		Email * string `json:"email" bidng:"omitempty",email`
+		Password *string `json:"password" binding:"omitempty,min=6"`
+	}
+
+	var user models.User
+	if err := config.DB.First(&user, userID).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"status": "error", "message": "User not found"})
+		return
+	}
+
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"status": "error", "message": "Body is required"})
+		return
+	}
+
+	updates := make(map[string]interface{})
+	if body.Name != nil {
+		updates["Name"] = strings.TrimSpace(*body.Name)
+	}
+	if body.Email != nil {
+		// Email cannot be changed at this time
+		c.JSON(http.StatusNotAcceptable, gin.H{"status": "error", "messsage": "Email can't be updated"})
+		return
+	}
+	if body.Password != nil {
+		hashedPassword, err := utils.HashPassword(*body.Password)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"status": "error", "message": "Failed to hash password"})
+			return
+		}
+		updates["Password"] = hashedPassword
+	}
+
+	if len(updates) == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"status": "error", "message": "No fields updated"})
+		return
+	}
+
+	result := config.DB.Model(&user).Updates(updates)
+
+	if result.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"status": "error", "message": "Failed to update user"})
+		return
+	}
+
+	if result.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"status": "error", "message": "No changes detected"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"status": "success", "message": "User updated successfully", "user": user})
+}
 
 func DeleteUser(c *gin.Context) {
 	userID := c.MustGet("userID").(uint)
--- routes/user.routes.go
+++ routes/user.routes.go
@@ -11,6 +11,6 @@ func UserRoutes(c *gin.RouterGroup) {
 	userRoute := c.Group("/users")
 
 	userRoute.GET("/:id", controller.GetUser)
-	userRoute.PUT("/me", utils.Protect(), controller.UpdateUser)
+	userRoute.PATCH("/me", utils.Protect(), controller.UpdateUser)
 	userRoute.DELETE("/me", utils.Protect(), controller.DeleteUser)
 }
